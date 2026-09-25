fab8410db40cb6b5853fa8220e5c0b5d8507f580
yruffieux <yann.ruffieux@ispm.unibe.ch>
2026-09-25 13:50:15 +0200

    probability differences (init cohort)

--- R/30_analysis_init_cohort.R
+++ R/30_analysis_init_cohort.R
@@ -1,6 +1,7 @@
 library(data.table)
 library(tictoc)
 library(mgcv)
+library(emmeans)
 
 filepath_load <- "C:/ISPM/Data/SolidarMed/processed"
 filepath_out <- "C:/ISPM/Data/SolidarMed/output"
@@ -9,11 +10,13 @@ load(file=file.path(filepath_load,"DTG_init_cohort.RData"))
 
 fup_grid <- seq(0,7,by=1/50)
 
+DT[,age_base_cat:=cut(age_base,breaks=c(0,20,30,40,50,70,Inf),right=FALSE)]
+
 ########## absolute probabilities
 
 pred_list <- list()
 
-pred_grid <- data.frame(delta_init=rep(fup_grid,2),district=factor(c(rep(0,length(fup_grid)),rep(1,length(fup_grid)))))
+pred_grid <- expand.grid(delta_init=fup_grid,district=factor(c(0,1)))
 
 for(prog in c("SMARTLES","SMARTMOZ","SMARTZIM"))
 {
@@ -40,3 +43,30 @@ for(prog in c("SMARTLES","SMARTMOZ","SMARTZIM"))
 }
 
 save(pred_list,file=file.path(filepath_out,"DTG_probs_init_cohort.RData"))
+rm(pred_list)
+
+###### adjusted probability differences for DTG uptake, over time
+
+pred_list <- list()
+
+for(prog in c("SMARTLES","SMARTMOZ","SMARTZIM"))
+{
+  DT_temp <- DT[program==prog]
+  DT_temp[,district_num:=as.numeric(NA)]
+  DT_temp[district%in%c("Butha-Buthe","Ancuabe","Bikita"),district_num:=0]
+  DT_temp[district%in%c("Mokhotlong","Chiure","Zaka"),district_num:=1]
+  DT_temp[,district:=factor(district_num)]
+  DT_temp[,district_num:=NULL]
+  
+  greg <- gam(dtg_ind ~ s(delta_init, by=district) + district + sex + age_base_cat, family=binomial(link="logit"), data=DT_temp)
+  emm <- emmeans(greg, ~district | delta_init, at = list(delta_init=fup_grid))    
+  emm <- regrid(emm)         # switching to probabilities
+  contr <- as.data.table(confint(contrast(emm, method = "revpairwise")))
+  
+  pred_list[[prog]] <- contr[,.(delta_init=as.numeric(as.character(delta_init)),Estimate=estimate,lower=lower.CL,upper=upper.CL)]
+  
+  rm(greg,DT_temp,contr)
+}
+
+save(pred_list,file=file.path(filepath_out,"DTG_probdiffs_init_cohort.RData"))
+rm(pred_list)
--- R/40_display_items_dtg_init_cohort.R
+++ R/40_display_items_dtg_init_cohort.R
@@ -44,5 +44,39 @@ pp <- pp_les + pp_moz + pp_zim + plot_layout(ncol=1, axis_titles="collect_y") &
   scale_x_continuous(breaks=0:7) &
   labs(y="Percentage initiating ART on DTG")
 
-ggsave(pp,filename=file.path(filepath_plot,"probs_DTG_init_cohort.png"),width=7,height=7,dpi=600)
+ggsave(pp,filename=file.path(filepath_plot,"DTG_probs_init_cohort.png"),width=7,height=7,dpi=600)
+rm(pp_les,pp_moz,pp_zim,pp)
+
+##### adjusted probability differences
+
+load(file=file.path(filepath_read,"DTG_probs_init_cohort.RData"))
+
+
+##### odds ratios
+
+load(file=file.path(filepath_read,"DTG_probdiffs_init_cohort.RData"))
+
+pp_les <- ggplot(pred_list[["SMARTLES"]],aes(x=delta_init,y=Estimate)) +
+  geom_line() +
+  geom_ribbon(aes(ymin=lower,ymax=upper),linetype=0,alpha=0.2) +
+  labs(x="Years since DTG adoption (2018-11-01)",title="(A) Lesotho: Mokhotlong vs. Butha-Buthe")
+
+pp_moz <- ggplot(pred_list[["SMARTMOZ"]],aes(x=delta_init,y=Estimate)) +
+  geom_line() +
+  geom_ribbon(aes(ymin=lower,ymax=upper),linetype=0,alpha=0.2) +
+  labs(x="Years since DTG adoption (2019-05-01)",title="(B) Mozambique: Chiure vs. Ancuabe")
+
+pp_zim <- ggplot(pred_list[["SMARTZIM"]],aes(x=delta_init,y=Estimate)) +
+  geom_line() +
+  geom_ribbon(aes(ymin=lower,ymax=upper),linetype=0,alpha=0.2) +
+  labs(x="Years since DTG adoption (2019-05-01)",title="(C) Zimbabwe: Zaka vs. Bikita")
+
+pp <- pp_les + pp_moz + pp_zim + plot_layout(ncol=1, axis_titles="collect_y") &
+  theme_bw() &
+  theme(panel.grid.minor=element_blank(),legend.position=c(0.8,0.4)) &
+  scale_y_continuous(labels=scales::percent,limits=c(-0.3,0.3)) &
+  scale_x_continuous(breaks=0:7) &
+  labs(y="Percent difference in percentage of DTG uptake")
+
+ggsave(pp,filename=file.path(filepath_plot,"DTG_probdiffs_init_cohort.png"),width=7,height=7,dpi=600)
 rm(pp_les,pp_moz,pp_zim,pp)
